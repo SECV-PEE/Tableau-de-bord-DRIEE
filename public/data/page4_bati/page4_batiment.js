3ff9b560be1e76b026b9fcb396d7623a430cd6ce
kberisha42 <kberisha42@student.42.fr>
2020-12-11 12:37:44 +0100

    first try DPE area chart

--- public/data/page4_bati/page4_batiment.js
+++ public/data/page4_bati/page4_batiment.js
@@ -1,9 +1,11 @@
 Promise.all([
-  d3.csv("data/page4_bati/DPE_IDF.csv")
+  d3.csv("data/page4_bati/DPE_IDF.csv"),
+  d3.csv("data/page4_bati/DPE_IDF_Conso.csv")
 ]).then((data)=>{
+  data_conso = data[1];
   data = data[0];
   drawBarDpe(conversor(data));
-  // drawAreaDpe(data);
+  drawAreaDpe(conversor_conso(data_conso));
 })
 
 function conversor(data) {
@@ -12,13 +14,17 @@ function conversor(data) {
   letters.forEach(element => {
     data[0][element] = +data[0][element].replace(" %", "")
   })
-  console.log(data);
   return (data[0])
 }
 
-function getRegionData(data)
-{
-  return (data.filter(function (d) {return d.Zone == "Région"})[0])
+function conversor_conso(data) {
+  var letters = ["Z", "A", "B", "C", "D", "E", "F", "G"];
+  letters.forEach(element => {
+    data[0][element] = +data[0][element].replace(" %", "")
+    data[1][element] = +data[1][element]
+  })
+  console.log(data)
+  return (data)
 }
 
 function showBatiTooltip_Dpe(category, value, coords)
@@ -130,10 +136,10 @@ function drawBarDpe(data) {
 }
 
 function drawAreaDpe(data) {
-  var margin = {top: 10, right: 30, bottom: 30, left: 50},
-      width = 460 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
-      
+  var margin = {top: 30, right: 30, bottom: 70, left: 30},
+    width = 460 - margin.left - margin.right,
+    height = 350 - margin.top - margin.bottom;
+
   var svg = d3.select("#dpe_area_chart")
     .append("svg")
       .attr("width", width + margin.left + margin.right)
@@ -142,5 +148,56 @@ function drawAreaDpe(data) {
       .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
-  svg.append()
+  var x = d3.scaleLinear()
+    .domain([0, 100])
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  var y = d3.scaleLinear()
+    .domain([0, 500])
+    .range([ height, 0 ]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+
+  var letters = ["Z", "A", "B", "C", "D", "E", "F", "G"];
+  var colorzZz = d3.scaleOrdinal().domain(letters)
+    .range(["#d3ee24", "#83BF74", "#22D55D", "#C9F970", "#FFFF3F", "#FFD72D", "#F3A24C", "#FF0100"])
+
+  var area = d3.area()
+  .x(function(d) { return x(d.x); })
+  .y0(height)
+  .y1(function(d) { return y(d.y); });
+  console.log(data.filter(function(d) {return y(d.y)}))
+
+  var last_x = x(0)
+  var last_y = y(0)
+
+  letters.forEach(element => {
+    console.log(data[1][element])
+    console.log("y pos = " + y(0))
+    console.log("x pos = " + x(0))
+    svg.append("path")
+      .datum(data)
+      .attr("fill", colorzZz(element))
+      .attr("stroke", colorzZz(element))
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.area()
+        .x(x(data[0][element]))
+        .y0(y(0))
+        .y1(y(data[1][element]))
+        )
+    svg.append("line")
+      .datum(data)
+      .attr("stroke", colorzZz(element))
+      .attr("stroke-width", 1.5)
+      .attr("x1", last_x)
+      .attr("x2", x(data[0][element]))
+      .attr("y1", last_y)
+      .attr("y2", y(data[1][element]))
+    last_x = x(data[0][element])
+    last_y = y(data[1][element])
+  });
 }
