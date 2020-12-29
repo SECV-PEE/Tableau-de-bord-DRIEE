48c1fbbbfe1e6ea64933088c54ff22c48a785317
kberisha42 <kberisha42@student.42.fr>
2020-12-29 21:02:46 +0100

    try v2

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,8 @@ echo 'climat-energie-idf.fr' > CNAME
 git init
 git config user.name "SECV-PEE"
 git config user.email "crcae.idf@gmail.com"
-git remote add origin https://SECV-PEE:Tableaubord2020!@github.com/SECV-PEE/Tableau-de-bord-DRIEE.git
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/SECV-PEE/Tableau-de-bord-DRIEE.git master:gh-pages
+git push -f git@github.com:SECV-PEE/Tableau-de-bord-DRIEE.git master:gh-pages
 
 cd -
