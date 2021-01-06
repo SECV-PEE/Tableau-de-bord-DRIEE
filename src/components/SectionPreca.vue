1a8076b919dcc5355a3b2577d2ea8d9fb48e4059
SECV-PEE <69907193+SECV-PEE@users.noreply.github.com>
2021-01-06 15:43:40 +0100

    Update SectionPreca.vue

--- src/components/SectionPreca.vue
+++ src/components/SectionPreca.vue
@@ -5,8 +5,10 @@
       </div>
       <DataContainer :path="`${path}${img_page}`" :data="data" v-bind:intro="false"></DataContainer>
       <h3>Facture moyenne annuelle de chauffage dans les EPCI d'Île-de-France</h3>
+      <a href="https://www.insee.fr/fr/statistiques/3678895" target="_blank">Source: INSEE ANALYSES ILE-DE-FRANCE</a>
       <img class="img-precarite" :src="`${path}${img_page}/facture_energetique.png`" style="width: 600px" alt="facture_energetique">
       <h3>Nombre et part de ménages vulnérables énergétiquement dans les EPCI d'Île-de-France</h3>
+      <a href="https://www.insee.fr/fr/statistiques/3678895" target="_blank">Source: INSEE ANALYSES ILE-DE-FRANCE</a>
       <img class="img-precarite" :src="`${path}${img_page}/precarite_idf.png`" alt="precarite_idf" style="width: 800px; height: 520px;">
       <!-- <div class="info-hover">
         <div class="info-block">
